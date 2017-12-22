4e12529492e0d1895ada6d6c8158f35806cba29e
Matheus Matos <m.miranda.m@hotmail.com>
2017-12-22 10:18:55 -0400

    Criação de função e add de imagem

--- src/pages/feed/feed.ts
+++ src/pages/feed/feed.ts
@@ -15,12 +15,17 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class FeedPage {
   public user_name:string = 'Matheus Matos cod';
-  
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  public AddTwoNumbers(number_01:number, number_02:number):void{
+  	alert(number_01 + number_02)
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad FeedPage');
+    //this.AddTwoNumbers(10, 90)
   }
 
 }
